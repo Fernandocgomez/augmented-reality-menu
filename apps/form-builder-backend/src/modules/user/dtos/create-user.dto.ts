4fa11e61430bc62cf81f02a1aef29d8447beb225
fernandocgomez <fernandocgomez@live.com>
2022-12-12 01:30:20 -0600

    added Lenght validation

--- apps/form-builder-backend/src/modules/user/dtos/create-user.dto.ts
+++ apps/form-builder-backend/src/modules/user/dtos/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { Length } from 'class-validator';
+
 export class CreateUserDto {
+    @Length(5, 16)
     username: string;
+
+    @Length(8, 20)
     password: string;
 }
--- apps/form-builder-backend/src/modules/user/user.controller.ts
+++ apps/form-builder-backend/src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './schemas/user.schema';
@@ -8,6 +8,7 @@ export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Post()
+    @UsePipes(ValidationPipe)
     async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
         return this.userService.createUser(createUserDto.username, createUserDto.password);
     }
