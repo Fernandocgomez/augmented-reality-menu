1e686400b94c6fec6ab77fb8b6e349b53bfecdc8
fernandocgomez <fernandocgomez@live.com>
2022-12-11 23:49:02 -0600

    fixed issue with serving the nest an angular app locally

--- apps/form-builder-backend/src/app/app.module.ts
+++ apps/form-builder-backend/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { MongooseModule } from '@nestjs/mongoose'
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/demo'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
