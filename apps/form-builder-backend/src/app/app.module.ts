13387f31eab99fd4a0c13a2e659e069201946029
fernandocgomez <fernandocgomez@live.com>
2022-12-14 17:16:37 -0600

    changed project name

--- apps/form-builder-backend/src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from '@visual-form-builder/nestjs-user';
-
-@Module({
-  imports: [UserModule, MongooseModule.forRoot('mongodb://localhost/demo')],
-})
-export class AppModule {}
