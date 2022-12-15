d8fe5db47bcc37294dce8af53b42f6d4306fd172
fernandocgomez <fernandocgomez@live.com>
2022-12-14 22:05:55 -0600

    implemented  hashing password functionality

--- libs/nestjs/restaurant-owner/src/lib/repositories/restaurant-owner.repository.ts
+++ libs/nestjs/restaurant-owner/src/lib/repositories/restaurant-owner.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 import { RestaurantOwner, RestaurantOwnerDocument } from '../schemas/restaurant-owner.schema';
 
@@ -7,11 +7,20 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class RestaurantOwnerRepository {
-    constructor(@InjectModel(RestaurantOwner.name) private userModel: Model<RestaurantOwnerDocument>) {}
+    constructor(@InjectModel(RestaurantOwner.name) private userModel: Model<RestaurantOwnerDocument>) { }
 
     async createRestaurantOwner(restaurantOwner: RestaurantOwner): Promise<RestaurantOwner> {
         const newRestaurantOwner = new this.userModel(restaurantOwner);
 
-        return newRestaurantOwner.save();
+        return newRestaurantOwner.save().catch((e) => {
+            throw new HttpException(
+                {
+                    statusCode: HttpStatus.BAD_REQUEST,
+                    message: `Username '${e.errors.username.value}' is already taken.`,
+                },
+                HttpStatus.BAD_REQUEST,
+                { cause: new Error(e) }
+            );
+        });
     }
 }
