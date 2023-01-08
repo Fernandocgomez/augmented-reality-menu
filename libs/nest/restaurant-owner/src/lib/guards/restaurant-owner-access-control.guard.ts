52c7962cbd8b0a18b741dbfc696fd4a295d007c9
fernandocgomez <fernandocgomez@live.com>
2023-01-08 17:54:01 -0600

    done with restaurant-ownwer access control guard

---
+++ libs/nest/restaurant-owner/src/lib/guards/restaurant-owner-access-control.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class RestaurantOwnerAccessControlGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const params = request.params;
+    const restaurantOwner = request.user;
+    
+    return params.id === restaurantOwner.id;
+  }
+}
