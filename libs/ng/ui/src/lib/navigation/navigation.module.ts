92295df8fe773cfea7c018a5bae347004354f071
fernandocgomez <fernandocgomez@live.com>
2023-01-12 16:34:02 -0600

    created header component

--- libs/ng/ui/src/lib/navigation/navigation.module.ts
+++ libs/ng/ui/src/lib/navigation/navigation.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 
+import { UiMaterialModule } from '../material/material.module';
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, UiMaterialModule],
+	declarations: [HeaderComponent],
+	exports: [HeaderComponent],
 })
 export class UiNavigationModule {}
