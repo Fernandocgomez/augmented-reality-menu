e772b58813b97e6d7266c43a824841bf887d0de1
fernandocgomez <fernandocgomez@live.com>
2023-01-16 12:53:08 -0600

    done

--- libs/ng/data-access/src/lib/login/+state/login.effects.spec.ts
+++ libs/ng/data-access/src/lib/login/+state/login.effects.spec.ts
@@ -4,7 +4,8 @@ import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { Actions, EffectsMetadata, getEffectsMetadata } from '@ngrx/effects';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { JwtLocalStorageService } from '@xreats/ng/shared';
 import { cold, hot } from 'jasmine-marbles';
 import { of, throwError } from 'rxjs';
 
@@ -13,7 +14,7 @@ import { LoginFailureResponseStub } from './../testing/login-failure-response.st
 import { LoginSuccessResponseStub } from './../testing/login-success-response.stub';
 import * as LoginActions from './login.actions';
 import { LoginEffects } from './login.effects';
-import { initialLoginState, LoginState } from './login.reducer';
+import { initialLoginState } from './login.reducer';
 
 describe('LoginEffects', () => {
 	let actions$: Actions;
@@ -21,9 +22,12 @@ describe('LoginEffects', () => {
 	let loginEffect: LoginEffects;
 	let metadata: EffectsMetadata<LoginEffects>;
 
-	let store: MockStore<LoginState>;
 	let loginService: LoginService;
 	let router: Router;
+	let jwtLocalStorageService: JwtLocalStorageService;
+
+	const username = 'username';
+	const password = 'password';
 
 	beforeEach(async () => {
 		TestBed.configureTestingModule({
@@ -31,22 +35,22 @@ describe('LoginEffects', () => {
 			providers: [
 				LoginEffects,
 				LoginService,
+				JwtLocalStorageService,
 				provideMockActions(() => actions$),
 				provideMockStore({ initialState: initialLoginState }),
 			],
 		});
 
+		jwtLocalStorageService = TestBed.inject(JwtLocalStorageService);
 		loginEffect = TestBed.inject(LoginEffects);
 		loginService = TestBed.inject(LoginService);
 		router = TestBed.inject(Router);
 		metadata = getEffectsMetadata(loginEffect);
-		store = TestBed.inject(MockStore) as MockStore<LoginState>;
+
+		window.localStorage.clear();
 	});
 
 	describe('loginRequestStart$', () => {
-		const username = 'username';
-		const password = 'password';
-
 		it('should emit Login Request Success action when the service call is successful', () => {
 			jest.spyOn(loginService, 'login').mockReturnValue(of(LoginSuccessResponseStub()));
 			const action = LoginActions.loginRequestStartAction({ username, password });
@@ -75,6 +79,19 @@ describe('LoginEffects', () => {
 	});
 
 	describe('loginRequestSuccess$', () => {
+
+		beforeEach(() => {
+			jest.spyOn(router, 'navigate').mockReturnValue(Promise.resolve(true));
+			jest.spyOn(jwtLocalStorageService, 'setAccessToken').mockImplementation(() => {
+				window.localStorage.setItem('access_token', LoginSuccessResponseStub().access_token);
+				return;
+			});
+
+			const action = LoginActions.loginRequestSuccessAction(LoginSuccessResponseStub());
+
+			actions$ = hot('a', { a: action });
+		});
+
 		it('should not return any action', () => {
 			expect(metadata.loginRequestSuccess$).toEqual(
 				expect.objectContaining({
@@ -84,20 +101,19 @@ describe('LoginEffects', () => {
 		});
 
 		it('should navigate to the dashboard', () => {
-			jest.spyOn(router, 'navigate').mockReturnValue(Promise.resolve(true));
-			const action = LoginActions.loginRequestSuccessAction(LoginSuccessResponseStub());
-
-			actions$ = hot('a', { a: action });
-
 			expect(loginEffect.loginRequestSuccess$).toBeObservable(
 				cold('a', { a: LoginActions.loginRequestSuccessAction(LoginSuccessResponseStub()) })
 			);
 			expect(router.navigate).toHaveBeenCalledWith(['/dashboard']);
 		});
 
-		// it('should set the JWT in the local storage', () => {
-		// 	null;
-		// });
+		it('should set the JWT in the local storage', () => {
+			expect(loginEffect.loginRequestSuccess$).toBeObservable(
+				cold('a', { a: LoginActions.loginRequestSuccessAction(LoginSuccessResponseStub()) })
+			);
+			
+			expect(jwtLocalStorageService.getAccessToken()).toEqual(LoginSuccessResponseStub().access_token);
+		});
 
 		// it('should dispatch the AuthenticatedRestaurantOwnerAction', () => {
 		// 	null;
