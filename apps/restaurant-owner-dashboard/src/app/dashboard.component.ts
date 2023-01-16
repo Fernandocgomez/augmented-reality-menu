800fa7f51d9ecd386dcea46bc549bf7165e4ecce
fernandocgomez <fernandocgomez@live.com>
2023-01-16 16:29:39 -0600

    created dummy dhashbard component

---
+++ apps/restaurant-owner-dashboard/src/app/dashboard.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'xreats-restaurant-owner-dashboard-root',
+    template: 'hello world'
+})
+export class DashboardComponent {}
